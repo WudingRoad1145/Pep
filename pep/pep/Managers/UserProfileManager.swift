dc4654983b411a5a4c03e93da40a03032bf9354f
Lora <69283023+lora-x@users.noreply.github.com>
2025-02-23 19:13:43 -0800

    add user profile manager

---
+++ pep/pep/Managers/UserProfileManager.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+class UserProfileManager: ObservableObject{
+    private let userNameKey = "userName"
+    private let ageKey = "age"
+    private let bodyPartKey = "bodyPart"
+    private let motivationKey = "motivation"
+    private let notificationPreferenceKey = "notificationPreference"
+    private let onboardedKey = "onboarded"
+    
+    var userName: String {
+        get { UserDefaults.standard.string(forKey: userNameKey) ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: userNameKey) }
+    }
+    
+    var age: Int {
+        get { UserDefaults.standard.integer(forKey: ageKey) }
+        set { UserDefaults.standard.set(newValue, forKey: ageKey) }
+    }
+    
+    var bodyPart: String {
+        get { UserDefaults.standard.string(forKey: bodyPartKey) ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: bodyPartKey) }
+    }
+    
+    var motivation: String {
+        get { UserDefaults.standard.string(forKey: motivationKey) ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: motivationKey) }
+    }
+    
+    var notificationPreference: String {
+        get { UserDefaults.standard.string(forKey: notificationPreferenceKey) ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: notificationPreferenceKey) }
+    }
+    
+    var onboarded: Bool {
+        get { UserDefaults.standard.bool(forKey: onboardedKey) }
+        set { UserDefaults.standard.set(newValue, forKey: onboardedKey) }
+    }
+
+    func logCurrentUserProfile() {
+        print("Logging current user profile:")
+        print("User Name: \(userName)")
+        print("Age: \(age)")
+        print("Body Part: \(bodyPart)")
+        print("Motivation: \(motivation)")
+        print("Notification Preference: \(notificationPreference)")
+        print("Onboarded: \(onboarded)")
+    }
+}
